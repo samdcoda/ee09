5245272c87beef5ba9a6146889efb1af2bdff42f
samdcoda <samdcoda@gmail.com>
2021-05-31 23:26:24 +0200

    Update script.php

--- script.php
+++ script.php
@@ -19,3 +19,12 @@
       }
     ?>
  
+mysqli_connect(serwer, user, password, db_name) //id_connection or FALSE
+mysqli_select_db(id_connection, db_name) // TRUE/FALSE
+mysqli_error(id_connection) //error text
+mysqli_close(id_connection) // TRUE/FALSE
+mysqli_query(id_connection, query1)
+mysqli_fetch_row(result_query1)
+mysqli_fetch_array(result_query1)
+mysqli_num_rows(result_query1)
+mysqli_num_fields(result_query1)
