dc1de63d8d8d329c07da0a5689578eea57fc39a7
samdcoda <samdcoda@gmail.com>
2021-05-31 22:33:11 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <meta charset="utf-8">
-  <title>Title</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <!-- link js and php files -->
- </head>
- <body>
-  <div id="header">
-   <p>Header</p>
-  </div>
-  <div id="left">
-   <h2>Left box</h2>
-   <form action="sript.php" method="post">
-   <label> Label text: <input type="text" name="text"></label> <input type="submit" value="Show">
-   </form>
-  </div>
-  <div id="right">
-   <h2>Right box</h2>
+
     <?php
      $conn = mysqli_connect('localhost', 'root', 'dbname');
       if(!conn)
@@ -37,10 +18,4 @@
        mysqli_close($conn);
       }
     ?>
-  </div>
-  <div id="footer">
-   <p><a class="footer" href="project.html">Project</a></p>
-   <p><!-- contact form -->contact us</p>
-  </div>
- </body>
-</html>
+ 
